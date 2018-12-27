9680b8ab903efa2c8155634acaa10c5e4bbb4197
AlainRizkallah <alain.rizkallah@isep.fr>
2018-12-27 22:43:00 +0100

    Update Script.R
    Logistic regression

--- Script.R
+++ Script.R
@@ -1,3 +1,21 @@
 #First Script
+
+### Exploratory analysis of the data set
+
 dataset=read.table(file = 'dataR2.csv',header = T, sep=",")
 attach(dataset)
+summary(dataset)
+plot(dataset)
+cor(dataset)
+
+### Logistic regression
+YNClassification <- factor(Classification, levels=c(1,2), labels=c("Healthy controls","Patients" ))
+log.fit=glm(YNClassification~Age+BMI+Glucose+Insulin+HOMA+Leptin+Adiponectin+Resistin+MCP.1,data=dataset,family = binomial)
+summary(log.fit)
+
+### KNN
+
+### LDA
+### QDA
+### Decision trees
+### Support Vector Machine
